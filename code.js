7a3864ac54b05ea0ff5fc7836f42b7cc79d7ace9
anriquegame <anriquegame@gmail.com>
2023-05-20 21:51:04 -0700

    product data

--- code.js
+++ code.js
@@ -1,4 +1,15 @@
 function openProduct(id){
-    var productURL = "products/productpage.html?"+id
+    var productURL = "products/productpage.html?id="+id
     window.open(productURL, "_self");
+}
+function changeInfo(){
+    var queryString = window.location.search;
+    var params = new URLSearchParams(queryString);
+    var name = params.get('id');
+    console.log(name)
+
+    var arrayProduct = data.find(function(obj) {
+        return obj.name === name;
+    });
+    alert(arrayProduct.name+arrayProduct.desc+arrayProduct.imgPath)
 }
---
+++ products/productinfo.js
@@ -0,0 +1,12 @@
+var data = [
+    {
+        "name":"ASUS",
+        "desc":"xyz",
+        "imgPath":"../img/ASUS.png"
+    },
+    {
+        "name":"LG",
+        "desc":"zyx",
+        "imgPath":"../img/LG.jpg"
+    }
+]
