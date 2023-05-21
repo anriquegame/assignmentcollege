ecaa595c69f4251e69225e9af45d3c4cac40fa06
anriquegame <anriquegame@gmail.com>
2023-05-21 00:24:37 -0700

    more products

--- products/productinfo.js
+++ products/productinfo.js
@@ -11,6 +11,13 @@ var data = [
         "fullName":"LG gram Style 2023 16Z90RS 16 inch ultra-lightweight OLED laptop, intel i7-1360P, 32GB RAM, 2TB SSD, Smart amp speakers & Dolby ATMOS, Windows 11 (Holographic White)",
         "desc":["[Beauty in Strength] The LG gram Style has beautiful yet durable Corning Gorilla Glass cover that changes colour in the light, inspired by the shimmering Auora skies","[OLED Display] Professional OLED Display that provides 16 inch Large screen, WQXGA(3200x2000) high resolution and DCI-P3 100%, HDR True Black 500 Low-reflection display which provides a smooth and stutter-free experience due to 0.2ms response time & 120Hz high refresh rate that can boost productivity","[Intel Evo] More powerful and faster performance with the Intel Evo Platform Powered by 13th Gen Intel Core Processors and Thunderbolt 4","[All-Day Battery life] 80Wh battery that's powered to last longer on a single charge","[Intel Unison] Do more with ease. This LG gram laptop is equipped with Intel Unison, which provides a seamless connectivity experience with your mobile regardless of Operating system such as Android, iOS, or Windows","[Dolby ATMOS] Dynamic Dolby Atmos audio and the LG gram's Smart Amp speakers deliver even crisper and clearer sound. So what's on screen not only looks stunning but is truly cinematic"],
         "imgPath":"../img/LG.jpg",
-        "price":"£289.99"
-    }
+        "price":"£290.00"
+    },
+    {
+        "name":"LENOVO",
+        "fullName":"Lenovo IdeaPad 3i 15 Inch Full HD Laptop (Intel Core i3-1115G4 Processor, 4GB RAM, 128GB Storage, Windows 10 Home S Mode) - Abyss Blue",
+        "desc":["YOUR DAILY COMPUTING COMPANION - Thin, light in a stylish blue body, powered by a reliable Intel processor","GREAT COMMUNICATIONS - Built in webcam with privacy shutter and stable connection through built in Intel WiFi 6","FOR WORK AND PLAY - 1 year subscription to Microsoft Office 365 for home and study - or run games like Roblox and Minecraft (not included)","BRIGHT DISPLAY - Full HD (1080 x 1920) anti-glare display with narrow bezels","EXPANDABLE - Connect to an external monitor, mice/keyboards and more with built-in headphone jack, USB Type-A and Type-C ports, HDMI port, and Bluetooth"],
+        "imgPath":"../img/LENOVO.jpg",
+        "price":"£300.00"
+    },
 ]
