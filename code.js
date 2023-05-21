b8e895134659ac0810b366baed153f677a3a9076
anriquegame <anriquegame@gmail.com>
2023-05-21 04:14:20 -0700

    list laptops

--- code.js
+++ code.js
@@ -1,12 +1,19 @@
-function openProduct(id){
-    var productURL = "products/productpage.html?id="+id
-    window.open(productURL, "_self");
+function openProduct(id,where){
+    if(where == 2){
+        var productURL = "productpage.html?id="+id
+        window.open(productURL, "_self");
+    }
+    else{
+        var productURL = "products/productpage.html?id="+id
+        window.open(productURL, "_self");
+    }
 }
+    
 function changeInfo(){
     var queryString = window.location.search;
     var params = new URLSearchParams(queryString);
     var name = params.get('id');
-    console.log(name)
+    console.log(name);
 
     var arrayProduct = data.find(function(obj) {
         return obj.name === name;
@@ -19,5 +26,20 @@ function changeInfo(){
         const li = document.createElement("li");
         li.textContent = item;
         document.getElementById("descP").appendChild(li);
+    });
+}
+function fillList() {
+    data.forEach(obj => {
+        var element = document.getElementById(obj.name);
+        element.querySelector("h2").textContent = obj.fullName;
+        element.querySelector("h3").textContent = obj.price;
+        element.querySelector("img").src = obj.imgPath;
+
+        var descList = element.querySelector("ul");
+        obj.desc.forEach(item => {
+            var li = document.createElement("li");
+            li.textContent = item;
+            descList.appendChild(li);
+        });
     });
 }
