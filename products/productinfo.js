9007d4496102358f24db018e34ca2eadb1ed718e
anriquegame <anriquegame@gmail.com>
2023-05-22 08:15:28 -0700

    laptop list finished

--- products/productinfo.js
+++ products/productinfo.js
@@ -4,14 +4,14 @@ var data = [
         "fullName":"ASUS Vivobook 15 X515JA 15.6 Full HD Laptop (Intel Core i3, 8GB RAM, 256GB PCIe SSD, Windows 11), Silver (X515JA-EJ2133W)",
         "desc":["15.6 Full HD screen","Powered by Intel's Core i3 dual-core processor","8GB RAM paired with 256GB PCIe SSD","UK layout keyboard","Microsoft Windows 11 Home operating system"],
         "imgPath":"../img/ASUS.png",
-        "price":"£2,650.00"
+        "price":"£300.00"
     },
     {
         "name":"LG",
         "fullName":"LG gram Style 2023 16Z90RS 16 inch ultra-lightweight OLED laptop, intel i7-1360P, 32GB RAM, 2TB SSD, Smart amp speakers & Dolby ATMOS, Windows 11 (Holographic White)",
         "desc":["[Beauty in Strength] The LG gram Style has beautiful yet durable Corning Gorilla Glass cover that changes colour in the light, inspired by the shimmering Auora skies","[OLED Display] Professional OLED Display that provides 16 inch Large screen, WQXGA(3200x2000) high resolution and DCI-P3 100%, HDR True Black 500 Low-reflection display which provides a smooth and stutter-free experience due to 0.2ms response time & 120Hz high refresh rate that can boost productivity","[Intel Evo] More powerful and faster performance with the Intel Evo Platform Powered by 13th Gen Intel Core Processors and Thunderbolt 4","[All-Day Battery life] 80Wh battery that's powered to last longer on a single charge","[Intel Unison] Do more with ease. This LG gram laptop is equipped with Intel Unison, which provides a seamless connectivity experience with your mobile regardless of Operating system such as Android, iOS, or Windows","[Dolby ATMOS] Dynamic Dolby Atmos audio and the LG gram's Smart Amp speakers deliver even crisper and clearer sound. So what's on screen not only looks stunning but is truly cinematic"],
         "imgPath":"../img/LG.jpg",
-        "price":"£290.00"
+        "price":"£2,650.00"
     },
     {
         "name":"LENOVO",
@@ -49,11 +49,81 @@ var data = [
         "price":"£2,500.00"
     },
     {
-        "name":"ALIENWARE",
-        "fullName":"Alienware m15 R7 15.6' QHD 240Hz Gaming Laptop, Intel Core i7-12700H, NVIDIA GeForce RTX 3080 Ti 16GB, 32GB RAM, 1TB SSD, NVIDIA G-SYNC, Windows 11 Home (Dark side of the moon)",
-        "desc":["【High Performance】The laptop is powered by Celeron N4020 with 256GB SSD 8GB RAM DDR4 to get the job done efficiently. Support TF card and 1TB SSD expansion, the large storage space allows you to perform daily tasks with peace of mind","【Visual Impact】With a 17.3 inch Full HD 1920×1080 IPS display and 2.8GHz refresh rate for smooth 4K video playback, enjoy a smooth, vibrant and dynamic experience on a vibrant, lifelike color screen","【Windows 11】 The 17 inch laptop is pre-installed with Windows 11, which supports various office and productivity software, and is compatible with various conventional and audio-visual entertainment applications. Great for work and entertainment. Rich interfaces such as 2xUSB 3.0, HDMI, Micro SD card Slot, etc., meet your connection needs.","【Slim and Lightweight】The sleek body weighs just 2kg and is only 2 cm thick, making it easy to carry around all day. Equipped with a 5000 mAh lithium-ion battery with up to 5 hours of battery life. It has a long-lasting battery and reliable performance. Perfect for an easy way to enjoy your lifestyle on the go. The SGIN 17" laptop is a powerful and reasonable laptop","【Worry-free After-sales Service】We provide 365 days warranty service from the date of purchase, if there is any problem, please contact us, we will reply you within 12 hours to help you solve the problem.
-        "],
-        "imgPath":"../img/ALIENWARE.jpg",
-        "price":"£2,500.00"
+        "name": "GEOBOOK",
+        "fullName": "GeoBook 2E 12.5-inch Laptop Windows 10 Intel Celeron 4GB RAM AC Wi-Fi 64GB eMMC",
+        "desc": [
+          "Slim and lightweight 12.5-inch laptop that brings portability to your workspace.",
+          "Pre-loaded with Windows 10 Pro offering enhanced productivity, security and privacy features.",
+          "Powered by an Intel Celeron processor, 4GB RAM and 64GB eMMC storage",
+          "Remote ready with a 2MP webcam, high quality dual microphones and dual built-in speakers.",
+          "Connectivity includes USB-C, 2x USB 3.0, Mini-HDMI, 3.5mm and MicroSD (up to 64GB) slot."
+        ],
+        "imgPath": "../img/GEOBOOK.jpg",
+        "price": "£99.99"
     },
+    {
+        "name": "SGIN",
+        "fullName": "SGIN 15.6 Inch Laptop Windows 11 8GB RAM 256GB SSD Celeron N4020 Up to 2.8GHz, PC Notebook with Dual Band WiFi, 2xUSB 3.0, Keyboard Film in Eight Languages (Black)",
+        "desc": [
+          "【High Performance】The laptop is powered by Celeron N4020 with 256GB SSD 8GB RAM DDR4 to get the job done efficiently. Support TF card and 1TB SSD expansion, the large storage space allows you to perform daily tasks with peace of mind",
+          "【Visual Impact】With a 15.6 inch HD 1366x768 pixels display and 2.8GHz refresh rate for smooth 4K video playback, enjoy a smooth, vibrant and dynamic experience on a vibrant, lifelike color screen",
+          "【Windows 11】 The 15.6 inch laptop is pre-installed with Windows 11, which supports various office and productivity software, and is compatible with various conventional and audio-visual entertainment applications. Great for work and entertainment. Rich interfaces such as 2xUSB 3.0, HDMI, Micro SD card Slot, etc., meet your connection needs.",
+          "【Slim and Lightweight】The sleek body weighs just 1.63 kg and is only 2 cm thick, making it easy to carry around all day. Equipped with a 5000 mAh lithium-ion battery with up to 8 hours of battery life. It has a long-lasting battery and reliable performance. Perfect for an easy way to enjoy your lifestyle on the go. The SGIN 15.6\" laptop is a powerful and reasonable laptop",
+          "【Worry-free After-sales Service】We provide 365 days warranty service from the date of purchase, if there is any problem, please contact us, we will reply you within 12 hours to help you solve the problem."
+        ],
+        "imgPath": "../img/SGIN.jpg",
+        "price": "£200.00"
+      },
+      {
+        "name": "JUMPER",
+        "fullName": "Jumper 16 Inch Laptop with Office 365, 1920x1200 FHD IPS Display (16:10), Intel Celeron J4105 Ultrabook, 4GB DDR4 128GB Laptops, Four Stereo Speakers, 38WH Battery, Dual-band Wi-Fi, Windows11 home",
+        "desc": [
+          "【Powerful Ultrabook】 The Jumper windows 11 pc notebook is powered by Celeron J4105 Quad core processor with a turbo frequency of 2.5GHz and has integrated Intel UHD 600 Graphics. It can smoothly load applications and provide stable performance. Additionally, the laptop is pre-installed with 1 year-Office 365 and Windows 11, an ideal laptop for basic home use and kids school work.",
+          "【Fullview HD Screen】 Jumper 16 inch laptop has high-definition display with an optimal resolution of 1920 x 1200 pixels. A stunning 16 : 10 widescreen display with 93% screen-to-body ratio gives you more space to stream, edit, and create.",
+          "【DIY Storage Available】 The Windows 11 laptop has 4GB LPDDR4 RAM, 128GB ROM Storage. It also supports 1TB SSD (M.2 SATA 2080) and 256GB TF card expansion, so you have enough space for large files and various streaming media you need.",
+          "【4 Stereo Speakers】 The Ultrabook comes with four high quality stereo speakers for a more immersive sound experience, and the large screen with stereo speakers makes watching videos as immersive as it is.",
+          "【Long Lasting Battery and High-Speed Transmission】 The laptop has a 38Wh large capacity battery, which lasts for 7 to 8 hours of comprehensive usage. Further, with the help of AC dual band WiFi, Bluetooth 4.0, standard USB3.0 and Type-C for easy connection to various peripherals and high-speed wireless transmission. Also, HDMI output for high definition audio, video."
+        ],
+        "imgPath": "../img/JUMPER.jpg",
+        "price": "£250.00"
+      },      
+      {
+        "name": "CHUWI",
+        "fullName": "CHUWI Herobook Pro Windows 11 Laptop, 14 inch 8GB RAM 256GB SSD(TF 512GB), FHD(1920x1080) 16:9 IPS Laptops, Intel N4020(1.1 to 2.8GHz), 4K Video, WiFi, BT4.0, Mini-HDMI Support Win11",
+        "desc": [
+          "[Powerful Processor laptops ] Herobook pro Genuine Windows 11 System laptop, features Intel Gemini lake N4020 processo up to 2.8 GHz. The latest generation UHD Graphics 600 and the highest frequency can reach 750MHz, excellent graphics processing capabilities to run hard decoding of 4K video smoothly.",
+          "[Large Capacity Storage] Notebook laptops with 8GB RAM+256GB SSD storage helps make multiple applications execute seamlessly. Up to 512 GB of TF expansion, enough space for files, photos, and videos. It provides wider space and higher read/write speeds for that e-books, songs, videos, photos, music.",
+          "[Multi-Function & Larger Battery] Our computer have large 38Wh polymer lithium ion battery last over 9 hours, built-in fast charging technology. Versatile connection, USB 3.0, USB 2.0, BT3.0, Micro-HD, high Speed M.2, Fast Charging DC Port. 5G dual-frequency WiFi to provide high-speed wireless network experience, to improve the data transmission.",
+          "[FHD Screen & Size Keyboard ] Herobook pro windows 11 laptops14.1 inch 1920X1080 Full HD IPS screen. Laptop weighs only 1.3kg and thick only 21.3mm (0.83 inch). Full-size keyboard, 5.75-inch oversized trackpad, delivers you a smooth touch and operation performance.",
+          "[Peace of Mind Warranty] CHUWI laptops have passed strict tests, one year warranty, please rest assured to buy. If you have any questions with goods, contact us, we will serve you wholeheartedly."
+        ],
+        "imgPath": "../img/CHUWI.jpg",
+        "price": "£180.00"
+      },
+      {
+        "name": "MSI",
+        "fullName": "MSI Pulse 15 Gaming Laptop (B13VFK-295UK), Latest Intel Core i7-13700H, 15.6\" QHD, 165Hz Panel, Latest GeForce RTX 4060, GDDR6 8GB, 16GB, 1TB SSD, Windows 11 - Titanium Gray",
+        "desc": [
+          "Intel 13th Gen CoreRaptor Lake i7-13700H",
+          "GeForceRTX 4060, GDDR6 8GB",
+          "1TB PCIe SSD",
+          "4-Zone RGB Gaming Keyboard",
+          "Cooler Boost 5"
+        ],
+        "imgPath": "../img/MSI.jpg",
+        "price": "£1,430.00"
+      },
+      {
+        "name": "SAMSUNG",
+        "fullName": "Samsung Galaxy Chromebook Go XE340XDA, 14” Laptop Chrome OS, Display Screen Full HD LED, RAM 4GB RAM, 32GB Storage, Silver [UK Version]",
+        "desc": [
+          "Thin and light laptop: Galaxy Chromebook Go makes for a perfect fit for on-the-go learners. Made for mobility, in a timeless silver hue, it's ready to go wherever learning takes you.",
+          "Fit for Education: With a 180° hinge, this is the perfect Chromebook for creative learning and with the sturdy and shock-resistant build it is built to last and stay protected against classrooms and beyond.",
+          "Rapid Connectivity: downloading, streaming, checking email, and remote learning can be done while on the move. Study and play wherever your day takes you, with its always on 4G LTE connection.",
+          "Power through the day: Even a full day of classes is no problem with Galaxy Chromebook Go's battery life lasting up to 12 hours on a single charge.",
+          "More Ports, More possibilities: versatile ports, including the USB Type-C, USB 3.2, SIM card tray, ear jack, and memory card slot, are set to bridge learning and technology in an ever changing world."
+        ],
+        "imgPath": "../img/SAMSUNG.jpg",
+        "price": "£300.00"
+      }      
 ]
