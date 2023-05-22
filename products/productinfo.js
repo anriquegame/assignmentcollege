f752b19f5c3307d12972ff9eab24477f1b31b22a
anriquegame <anriquegame@gmail.com>
2023-05-22 12:19:31 +0100

    day on college more pages

--- products/productinfo.js
+++ products/productinfo.js
@@ -20,4 +20,40 @@ var data = [
         "imgPath":"../img/LENOVO.jpg",
         "price":"£300.00"
     },
+    {
+        "name":"DELL",
+        "fullName":"Dell Inspiron Laptop, 15.6' HD Display, AMD Ryzen 5 3450U Processor, 32GB RAM, 1TB SSD, Webcam, HDMI, SD Card Reader, Wi-Fi, Windows 11 Home, Carbon Black",
+        "desc":["【High Speed RAM And Enormous Space】32GB high-bandwidth DDR4 RAM to smoothly run multiple applications and browser tabs all at once; 1TB PCIe Solid State Drive for ample storage space","【Processor】AMD Ryzen 5 3450U Processor (4 Cores, 8 Threads, 4MB L3 Cache, Base Clock at 2.1GHz, Up to 3.5GHz at Max. Boost Clock), AMD Radeon Graphics","【Display】15.6', HD 1366x768, 60Hz, Non-Touch, Anti-Glare, LED-Backlit, Narrow Border","【Tech Specs】2 x USB 3.2 Gen 1, 1 x USB 2.0, 1 x Audio Jack, 1 x HDMI 1.4, 1 x SD Card Reader; 802.11ac 1x1 Wi-Fi and Bluetooth","【Operating System】Windows 11 Home - Beautiful, more consistent new design, Great window layout options, Better multi-monitor functionality, Improved performance features, New videogame selection and capabilities, Compatible with Android Apps"],
+        "imgPath":"../img/DELL.jpg",
+        "price":"£800.00"
+    },
+    {
+        "name":"HP",
+        "fullName":"HP 15.6' Full HD Laptop 15s-eq1048na, AMD Ryzen 7, 16GB RAM, 512GB SSD, Windows 11 - Natural Silver",
+        "desc":["Power through your day. Stay connected to what matters most with long-lasting battery life and a thin micro-edge bezel design. Built to keep you productive and entertained from anywhere, the HP 39.6 cm (15.6') Laptop features reliable performance and an expansive display, so you can stream, surf and speed through tasks","View more, carry less. Thin, light, and designed to travel. See more of your photos, videos and projects on a 6.5 mm micro-edge bezel display","Reliable performance. Tackle your busiest days with the performance of a reliable processor. Save more of what you love with abundant storage for all your photos, videos and documents","Go longer, faster. Long-lasting battery life and HP Fast Charge technology lets you work, watch, and stay connected all day. Integrated precision touchpad with multitouch support speeds up navigation and productivity"],
+        "imgPath":"../img/HP.jpg",
+        "price":"£550.00"
+    },
+    {
+        "name":"ACER",
+        "fullName":"Acer Aspire 5 A515-57G 15.6 Inch Laptop - (Intel Core i5-1235U, 16GB, 512GB SSD, NVIDIA GeForce MX550, Full HD Display, Windows 11, Iron)",
+        "desc":["POWERFUL PERFORMANCE: The combined performance of a 12th Gen Intel Core i5 CPU and 16GB of RAM means you'll breeze through the most demanding of tasks","ENHANCED GRAPHICS: With a dedicated Nvidia GeForce MX550 GPU you can take on more visually-demanding challenges like photo editing","VISIBLY STUNNING: The 15.6 Inch Full HD IPS screen combines incredibly sharp detail, vivid lifelike colours and wide viewing angles for a brilliant visual experience","PREMIUM CHASSIS: The aluminium body features a backlit keyboard and raised hinge design to provide a more comfortable typing position, clearer audio and better cooling","WORK FROM AN YWHERE: The HD webcam with TNR technology and dual microphones with AI noise reduction allow you to video call from anywhere without worrying about being seen or heard clearly"],
+        "imgPath":"../img/ACER.jpg",
+        "price":"£600.00"
+    },
+    {
+        "name":"ALIENWARE",
+        "fullName":"Alienware m15 R7 15.6' QHD 240Hz Gaming Laptop, Intel Core i7-12700H, NVIDIA GeForce RTX 3080 Ti 16GB, 32GB RAM, 1TB SSD, NVIDIA G-SYNC, Windows 11 Home (Dark side of the moon)",
+        "desc":["Experience elite performance across your favorite games and applications with 12th Gen Intel Core processors","Graphics designed for maximum performance with NVIDIA GeForce RTX","Gain the edge you need with Alienware Cryo-tech cooling technology","Experience smooth, fast, and stunning graphics as the developers intended with more contrast and vivid colours that go beyond the traditional high dynamic range (HDR)","Keep your vision sharp and relaxed with ComfortView Plus, an always on built-in low blue light screen that reduces blue light emissions while delivering excellent colour without effecting performance or image quality"],
+        "imgPath":"../img/ALIENWARE.jpg",
+        "price":"£2,500.00"
+    },
+    {
+        "name":"ALIENWARE",
+        "fullName":"Alienware m15 R7 15.6' QHD 240Hz Gaming Laptop, Intel Core i7-12700H, NVIDIA GeForce RTX 3080 Ti 16GB, 32GB RAM, 1TB SSD, NVIDIA G-SYNC, Windows 11 Home (Dark side of the moon)",
+        "desc":["【High Performance】The laptop is powered by Celeron N4020 with 256GB SSD 8GB RAM DDR4 to get the job done efficiently. Support TF card and 1TB SSD expansion, the large storage space allows you to perform daily tasks with peace of mind","【Visual Impact】With a 17.3 inch Full HD 1920×1080 IPS display and 2.8GHz refresh rate for smooth 4K video playback, enjoy a smooth, vibrant and dynamic experience on a vibrant, lifelike color screen","【Windows 11】 The 17 inch laptop is pre-installed with Windows 11, which supports various office and productivity software, and is compatible with various conventional and audio-visual entertainment applications. Great for work and entertainment. Rich interfaces such as 2xUSB 3.0, HDMI, Micro SD card Slot, etc., meet your connection needs.","【Slim and Lightweight】The sleek body weighs just 2kg and is only 2 cm thick, making it easy to carry around all day. Equipped with a 5000 mAh lithium-ion battery with up to 5 hours of battery life. It has a long-lasting battery and reliable performance. Perfect for an easy way to enjoy your lifestyle on the go. The SGIN 17" laptop is a powerful and reasonable laptop","【Worry-free After-sales Service】We provide 365 days warranty service from the date of purchase, if there is any problem, please contact us, we will reply you within 12 hours to help you solve the problem.
+        "],
+        "imgPath":"../img/ALIENWARE.jpg",
+        "price":"£2,500.00"
+    },
 ]
