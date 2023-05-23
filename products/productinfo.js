62ca3b087b2f4511b644ecbaed2107aa9c179d1e
anriquegame <anriquegame@gmail.com>
2023-05-23 06:57:47 -0700

    phone products

--- products/productinfo.js
+++ products/productinfo.js
@@ -125,5 +125,169 @@ var data = [
         ],
         "imgPath": "../img/SAMSUNG.jpg",
         "price": "£300.00"
-      }      
+      },
+      {
+        "name": "APPLE",
+        "fullName": "Apple iPhone 14 Plus (128 GB) - Midnight",
+        "desc": [
+          "6.7-inch Super Retina XDR display",
+          "All-day battery life and up to 26 hours of video playback",
+          "Industry-leading durability features with Ceramic Shield and water resistance",
+          "A15 Bionic chip with 5-core GPU for lightning-fast performance. Superfast 5G cellular",
+          "Advanced camera system for better photos in any light",
+          "Action mode for smooth, steady handheld videos",
+          "Cinematic mode now in 4K Dolby Vision up to 30 fps"
+        ],
+        "imgPath": "../img/APPLE.jpg",
+        "price": "£860.00"
+      },
+      {
+        "name": "OPPO",
+        "fullName": "OPPO A94 5G - 8GB RAM and 128 +Extendable Storage SIM Free Smartphone (48MP AI Quad Camera, 6.4' AMOLED Screen, 30W fast charge) - Fluid Black",
+        "desc": [
+          "Super-fast 5G Internet featuring dual 5G SIM cards and an eight-core 5G processor with up to 2.4 GHz CPU frequency.",
+          "About 173g light and 7.8mm thin, feel the smooth sleek design of one of the thinnest 5G phones available.",
+          "OPPO`s proprietary VOOC Flash Charge combined with a 4310mAh large battery allows you to power up quickly, while VOOC Flash Charge is especially useful for emergency low-battery situations.",
+          "Ultra Night Video Quad Camera,Combined with the large 48MP sensor, OPPO`s proprietary Ultra Night Video Algorithm dramatically improves night-time video performance. Night videos are not only significantly brighter, but also with better dynamic range and more vivid color.",
+          "Connector Type: USB Type C"
+        ],
+        "imgPath": "../img/OPPO.jpg",
+        "price": "£200.00"
+      },
+      {
+        "name": "ULEFONE",
+        "fullName": "Ulefone Mobile Phones SIM Free Unlocked, 8GB RAM 64GB ROM, Note 14, Android 12 Phone, 4G Dual SIM Smartphone, Triple Card Slots, 4500mAh Battery, 13MP Camera, 6.52 inch Screen, Midnight Black",
+        "desc": [
+          "Up to 8GB RAM (internal set with 4GB virtual RAM) + 64GB ROM storage as well as MediaTek Helio A22 processor and support external 128GB MicroSD card storage. [*Please update to the newest system to set 8GB RAM virtual memory.]",
+          "4G dual SIM dual standby. Compatible with EE, O2, Vodafone, Three (TRE), Virgin, Giff Gaff, Orange, T-mobile, Sky Mobile, iD Mobile and more compatible networks.",
+          "The newest Android 12 system. Deliver even more personal, safe and effortless experiences on your device. Featuring a totally reimagined UI just for you, new privacy.",
+          "High capacity of 4500mAh, with OTG function enables it not only directly connect for data transmission also supports reverse charging. USB Type-C reversible connector port.",
+          "3-Card independent slots design, compatible with dual Nano SIM and one TF card, which could be used at the same time, allowing you flexibility to switch between SIM cards at any time.",
+          "13MP main camera and 5MP front camera, you can capture all your favorite moments. And HDR can help you take better-looking photos.",
+          "6.52 inch waterdrop HD+ screen, delivers incomparable artistic shades feeling. 8.95mm 3D slim body, 20:9 Aspect Ratio delivers superior hand feel."
+        ],
+        "imgPath": "../img/ULEFONE.jpg",
+        "price": "£100.00"
+      },
+      {
+        "name": "DOOGEE",
+        "fullName": "DOOGEE X97 SIM-Free & Unlocked Mobile Phones, Android 12 Smartphone, 3GB+16GB, 256GB Extension, 4G Dual SIM Cheap Phone, 3-Card Slots, 6.0'' HD+ Display, 4200mAh, Face ID, GPS, Type C UK Version-Green",
+        "desc": [
+          "【The Latest Android 12 System】As the most cost-effective Android smartphone, the X97 mobile phone is equipped with the latest Android 12 system to deliver even more personal, safe and effortless experiences. Featuring a totally reimagined UI just for you, pretty icons, adjustable size of fonts. This phone is ideal for spare phone, children phone, elderly phone etc.",
+          "【6.0''HD+ Display+Ultra-Slim Body】DOOGEE X97 mobile phone has 6.0''HD+ display with 720*1440HD+ Resolution and 89% Screen-to-body Ratio, it opens the door to the virtual world with excellent lifelike colors and stunning screen clarity. The brightness of 420nits allows you to read the contents of your phone clearly even in the sun. 9.7mm Slim ultra-thin body design, Fascinating, Aesthetically, Ergonomic, perfectly fitting into the palm of a hand for a relaxed grip.",
+          "【4200mAh Battery+2.0GHz Low Power Processor】DOOGEE X97 android phone is equipped with an extraordinary 4200mAh battery, delivering long-lasting battery life. Call at 20H, listen to music at 16H, play games at 5H, and stand by at 240H. Equipped with a low power consumption 2.0GHz processor, even that you`re a heavy smartphone user, it still lasts longer than you can expect.",
+          "【2+1-Card Slots Design & 4G LTE】 The DOOGEE X97 unlocked phone uses a 2+1 card slot design, which can use 2 SIM cards and 1 micro SD card at the same time (X97 smartphones can support 256GB of extended memory). 4G LTE Connectivity, provides a fast and stable mobile network, and has better experience in work and gaming capabilities. Support 3.5mm headphone jack, WIFI, Bluetooth 5.0, GPS, FM and other functions to make your life more convenient.",
+          "【AI Dual Camera+ 2 Years Warranty】5MP front camera and 8MP rear AI camera, you can use filters, emojis or other technologies to further edit your photos, and it supports multiple shooting modes such as time-lapse, and professional. It only takes a second to unlock the phone with a face! We will provide 2 years warranty and professional after-sales service for the 4G Dual SIM free mobile phone X97!"
+        ],
+        "imgPath": "../img/DOOGEE.jpg",
+        "price": "£70.00"
+      },
+      {
+        "name": "NOKIA",
+        "fullName": "Nokia X10 6.67 Inch Android UK SIM Free Smartphone with 5G Connectivity - 6 GB RAM and 64 GB Storage (Dual SIM) - Forest Green",
+        "desc": [
+          "Work on the go with a Full HD+ screen",
+          "Blazing connections with 5G",
+          "48MP camera with ZEISS optics",
+          "Up to 2-day battery life",
+          "3 years of Android software updates",
+          "Everything you need to power your business"
+        ],
+        "imgPath": "../img/NOKIA.jpg",
+        "price": "£160.00"
+      },
+      {
+        "name": "XIAOMI",
+        "fullName": "Xiaomi Redmi 10 (2022) - Smartphone 128GB, 4GB RAM, Dual Sim, Carbon Gray",
+        "desc": [
+          "Screen: 6.5\", 1080 x 2400 pixels",
+          "Processor: Mediatek Helio G88 2GHz",
+          "Camera: Quad, 50MP+8MP+2MP+2MP",
+          "Battery: 5000mAh"
+        ],
+        "imgPath": "../img/XIAOMI.jpg",
+        "price": "£140.00"
+      },
+      {
+        "name": "FLIP",
+        "fullName": "Samsung Galaxy Z Flip4 5G Smartphone Sim Free Android Folding Phone 256GB, Pink Gold, 3 Year Manufacturer Warranty, UK Version",
+        "desc": [
+          "Choose a color that goes with everything - A hazy finish on the glass creates a soft contrast to the high-shine, glossy metal frame that is compact enough to fit in your palm. Select from four shades: Bora Purple, Graphite, Pink Gold and Blue.",
+          "Toughest Flip ever - From the inside out, Galaxy Z Flip4 is made to be durable. These are our toughest foldables ever with exclusive Corning Gorilla Glass Victus +. And the hinge is protected by Armor Aluminum.",
+          "Camera that captures your life - With FlexCam take advantage of your phone`s agile angles to capture group shots, selfies and videos. With Quick Shot take selfies with the Rear Camera while the Cover Screen gives you a real-time preview.",
+          "Power to the pocket - With this much juice, you can let every call go into overtime. An upgraded 4nm processor works with the long-lasting 3700mAh battery to power through the hours.",
+          "Custom cover screen - With a customizable 1.9-inch Cover Screen, you get yet another canvas to play with. Change it up as much as you want using your videos, photos or your own AR emoji to personalize the clock face."
+        ],
+        "imgPath": "../img/FLIP.jpg",
+        "price": "£1,060.00"
+      },
+      {
+        "name": "LNGLST",
+        "fullName": "LngLst Cheap Mobile Phones, Android OS, 5.0 inch Dual SIM Dual Camera, 4GB ROM (Expandable up to 128GB) Basic Smartphone (M10PLUS-Blue)",
+        "desc": [
+          "【5.0 inch IPS Display】 LngLst M10Plus as a cheap but functional android mobile phone, equipped with a 5.0 inch 18:9 IPS Display. the 800*480 resolution makes all graphics crisp and clear. The screen of LngLst Y30S sim free unlocked smartphone allowing you to get a more immersive experience when watching movies or playing games.",
+          "【3G Dual SIM & One Year Warranty】 LngLst M10Plus SIM-free unlocked phone supports dual SIM cards, you can insert two SIM cards at the same time to receive calls and SMS without the need for a second phone or switching SIM cards back and forth. We offer a one year warranty on the LngLst M10Plus. If you have trouble using the phone or have any other questions, please feel free to contact us and our customer service team will get back to you shortly.",
+          "【User Set Password to Unlock & Android 6.0】 LngLst M10Plus supports Password to Unlock, ensures the security of your information. LngLst M10Plus 3G dual sim mobile phone runs on Android 6.0, improving every aspect of the system, making the APP run smoother and providing many features to enhance system security and make life easier. (Android system cannot be upgraded.)",
+          "【Dual Camera & 128GB Extension】 Equipped with a 5MP rear camera and a 5MP front camera, the LngLst M10Plus android smartphones can take clear and vivid photos. 4GB ROM, 128GB expansion (SD card not included), you can use LngLst Y30S 3G sim free unlocked smartphone to save all your memorable moments such as photos, videos and messages without worrying about running out of storage space.",
+          "【2800mah large battery + 3D ultra-thin body design 】 As the cheapest but most durable Android phone, LngLst M10Plus is equipped with a large 2800 Ma battery, which can avoid the embarrassment of low battery power when going out. In addition, this phone also has an ultra-thin 3D body design, which makes it more comfortable to use!"
+        ],
+        "imgPath": "../img/LNGLST.jpg",
+        "price": "£50.00"
+      },
+      {
+        "name": "HONOR",
+        "fullName": "HONOR X6 Mobile Phone, 6.5 Inch Dual SIM Unlocked Smartphone, 50MP Triple Camera, 5000mAh, 4GB+64GB, Android 12",
+        "desc": [
+          "【Wonderful Photo Experience】The HONOR X6 features a rear triple camera system comprising a 50MP Main Camera, a 2MP Macro Camera, and a 2MP Depth Camera. The impressive combination of cameras allows users to easily capture and record all their favorite memories, capture the excellent moments of your life with ease",
+          "【FullView Display】Boasting a 6.5-inch HONOR FullView display, the HONOR X6 delivers an immersive viewing experience. Coupled with a host of eye care features including Eye Comfort Mode, and Dark Mode, it offers an extremely pleasant viewing experience in different types of user scenarios, perfect for reading articles, streaming videos, or browsing photos",
+          "【Long-Lasting Battery】Equipped with a 5000mAh battery, the HONOR X6 delivers up to 17 hours of online video streaming, 18 hours of social media browsing, or 31 hours of music playback, all enabled by a single charge to power users throughout the day",
+          "【Google Mobile Services】Smooth performance's MagicUI 6.1 based on Android 12, with all Google apps and play store, bringing all your favorite apps",
+          "【Expandable Storage 】HONOR X6 has 64GB storage and also supports large expandable storage of up to 1TB MicroSD (NOT INCLUDED), download your favorite content at any time, without ever having to delete your beautiful photos"
+        ],
+        "imgPath": "../img/HONOR.jpg",
+        "price": "£110.00"
+      },
+      {
+        "name": "GALAXY",
+        "fullName": "Samsung Galaxy M13 Mobile Phone SIM Free Android Smartphone 4GB RAM 64GB Storage Light Blue",
+        "desc": [
+          "More room to play. The 6.6 Inch Infinity-V Display and FHD+ technology makes everyday content look sharp, crisp and clear.",
+          "Do more, do now, do better. The Galaxy M13 combines octa-core processing power with up to 4GB RAM for overflowing energy in whatever you love to do.",
+          "Snap memorable moments in clear detail with the 50MP Main Camera. Expand the viewing angle with Ultra Wide Camera, or get closer to the details with Macro Camera.",
+          "The Galaxy M13`s quality design comes with soft, rounded edges and a simple core frame that houses a minimal camera look, coming in a range of colours so you can choose which goes best with your everyday style.",
+          "With 64/128GB of internal storage for your photos and videos, plus support for a microSD card up to 1TB, you'll get lots of space to store your precious moments."
+        ],
+        "imgPath": "../img/GALAXY.jpg",
+        "price": "£170"
+      },
+      {
+        "name": "BLACKVIEW",
+        "fullName": "Blackview Mobile Phones, A52 Android 12 Phone, 3GB+32GB/TF 1TB, Octa-Core, 13MP+5MP Cameras, 5180mAh, 4G Smart Phones Sim Free Unlocked, 6.517 inch HD+ IPS Screen, Face & Fingerprint Unlock - Blue",
+        "desc": [
+          "【Latest Android 12 GO + DOKE OS 3.0】A52 mobile phones is equipped with the latest Android 12 GO combined with Blackview's unique DOKE OS 3.0, bringing smoother, more responsive and more efficient performance. Android 12 Go adds privacy monitoring, and privacy protection is comprehensively enhanced. Doke OS 3.0 has built-in functions such as long screenshot, cold room, clone phone, smart floating windows, system manager, etc., which fits the user's usage habits and improves the user experience.",
+          "【3+32GB/1TB Expandable & Octa-Core】A52 smartphone is equipped with a low-power octa-core processor, combined with the newly upgraded RAM expansion technology, adding 1GB virtual memory on the basis of the original 2GB RAM, realizing 3GB RAM, which increases the running speed by 15%, providing enough performance for the phone to run top Android applications smoothly. With 32GB ROM and support for 1TB memory expansion, the ample storage space allows you to store massive files and pictures at will.",
+          "【13MP+5MP Cameras & Multiple Camera Modes】Blackview A52 mobile phones sim free unlocked are equipped with a 13MP ultra-clear main camera, giving you the clearest and most vivid color shooting experience. Experience panorama mode, portrait mode, professional photography, time-lapse photography and other photo modes to stimulate your creativity. 5MP front camera can be combined with the beauty selfie mode to fully show your personality. A52 is a stylish and powerful budget smartphone for everyone!",
+          "【5180mAh Battery & 6.517 inch HD+ IPS Screen & 4G Dual SIM】Blackview A52 Android 12 phones is equipped with a large 5180mAh battery, supports 5V/2A fast charging and OTG reverse charging. The battery life is beyond your imagination. 6.517-inch 720*1600 HD+ IPS display, restore the real vision. A52 4G Dual SIM smartphone supports 2 Nano SIM cards or 1 Nano SIM card + 1 TF card. With the A52's fast 4G LTE network, everything is going on at top speed. Let Blackview A52 phones be your life partner!",
+          "【Face+Fingerprint Unlock】A52 smart phones lets your unique face be your key to unlock your phone quickly and safely. 2-in-1 power button and fingerprint sensor, just touch the button with your finger to unlock your phone instantly! It only takes 0.1 seconds to unlock! A52 uses smooth external lines to enhance the texture of the cell phone, making it lightweight and advanced. 2 years warranty, please contact us if you have any questions. A52 phones have more surprises waiting for you to discover!"
+        ],
+        "imgPath": "../img/BLACKVIEW.jpg",
+        "price": "£80"
+      },
+      {
+        "name": "PIXEL",
+        "fullName": "Google Pixel 7 - Unlocked Android 5G Smartphone with wide-angle lens and 24-hour battery - 128GB - Obsidian",
+        "desc": [
+          "Google Tensor G2 makes Pixel 7 faster, more efficient and more secure - with the best photo and video quality yet on Pixel",
+          "The Adaptive Battery can last over 24 hours. Turn on Extreme Battery Saver, and the battery can last up to 72 hours",
+          "The Pixel 7 camera takes incredible, authentic photos with Real Tone and stunning, dramatic video with Cinematic Blur",
+          "With Google Tensor G2 and the Titan M2 security chip, your Pixel is built with multiple layers of security to help keep your personal info safe",
+          "With VPN by Google One built in, Pixel helps to protect your online activity no matter what app or web browser you use",
+          "With Live Translate, you can interpret face-to-face conversations in 48 languages, translate menus with the camera and translate messages in real time",
+          "The Pixel 7 display is super sharp, with rich, vivid colours. And it's fast and responsive for smoother gaming, scrolling and moving between apps"
+        ],
+        "imgPath": "../img/PIXEL.jpg",
+        "price": "£470"
+      }
+      
+      
 ]
